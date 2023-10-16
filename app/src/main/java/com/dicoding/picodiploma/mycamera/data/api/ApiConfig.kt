777de1b474f0c475ae392ed423ee43ed21caeddc
Furqon Nurbaril Yahya <furqonyahya1@gmail.com>
2023-10-16 11:35:48 +0700

    add BaseUrl

--- app/src/main/java/com/dicoding/picodiploma/mycamera/data/api/ApiConfig.kt
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/data/api/ApiConfig.kt
@@ -3,8 +3,9 @@ package com.dicoding.picodiploma.mycamera.data.api
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class ApiConfig {
+object ApiConfig {
     fun getApiService(): ApiService {
         val loggingInterceptor =
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
@@ -12,6 +13,10 @@ class ApiConfig {
             .addInterceptor(loggingInterceptor)
             .build()
         val retrofit = Retrofit.Builder()
-            .baseUrl()
+            .baseUrl("https://story-api.dicoding.dev/v1/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(ApiService::class.java)
     }
 }
